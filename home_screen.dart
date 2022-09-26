76ca51f329c171fafd28446792876bbe04d6d300
Ismail979797 <106625769+Ismail979797@users.noreply.github.com>
2022-09-27 02:13:15 +0600

    Add files via upload

---
+++ home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width / 3,
+          color: Colors.amber,
+        )
+      ],
+    ));
+  }
+}
